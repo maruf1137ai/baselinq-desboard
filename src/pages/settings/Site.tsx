28b08d5f49fdd64ed76357cef9cb0d9bac512183
Saif Hasan <saifhassan399@gmail.com>
2026-01-20 15:41:02 +0600

    Site page added

--- src/pages/settings/Site.tsx
+++ src/pages/settings/Site.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useProjects } from "@/supabse/hook/useProject";
+import { useEffect, useState } from "react";
+
 
 const Site = () => {
-  return <div>Site</div>;
+    const { data: projects = [], isLoading } = useProjects();
+   const [selectedProjectId, setSelectedProjectId] = useState(() =>
+      localStorage.getItem("selectedProjectId") || ""
+    );
+    const [currentProject , setCurrentProject] = useState<any>(null)
+  
+    useEffect(() => {
+     if(isLoading) return
+     setCurrentProject(projects.find((p: any) => p.id === selectedProjectId))
+    }, [projects, isLoading, selectedProjectId]);
+    
+    console.log('currentProject', currentProject)
+  
+  return  <div className="p-6">
+     <h2 className="text-[23px] text-[#1A1A1A] mb-2">Site Settings</h2>
+      <p className="text-[#6B7280] text-base mb-6">
+       Manage site location, photos, and environmental settings.
+      </p>
+  </div>
 };
 
 export default Site;
